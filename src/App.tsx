0dad25aa05e706aa95173916785d93cb5fd2fa13
Shuyue Zhang <shuyuezhang@macbook-pro.mynetworksettings.com>
2025-01-16 02:04:03 -0500

    a4 html fa34

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,56 @@
-import { useState } from "react";
-import Labs from "./Labs";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-// a1
-function App() {
-  const [count, setCount] = useState(0);
+// import { useState } from "react";
+// import Labs from "./Labs";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+// // a1
+// function App() {
+//   const [count, setCount] = useState(0);
 
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Welcome to Web Dev</h1>
+//       <div>
+//         <Labs />
+//       </div>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import Labs from "./Labs";
+import Kambaz from "./Kambaz";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+export default function App() {
   return (
-    <>
+    <HashRouter>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kambaz/*" element={<Kambaz />} />
+        </Routes>
       </div>
-      <h1>Welcome to Web Dev</h1>
-      <div>
-        <Labs />
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </HashRouter>
   );
 }
-
-export default App;
