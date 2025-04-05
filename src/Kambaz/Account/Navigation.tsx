8823d374c818c08b1f1c73f472b33cfc30cb3ce5
Shuyue Zhang <shuyuezhang@macbook-pro.mynetworksettings.com>
2025-04-05 15:40:12 -0400

    react web a6

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -9,40 +9,63 @@
 //   );
 // }
 
-import { Link} from "react-router-dom";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+
 export default function AccountNavigation() {
-  // const { currentUser } = useSelector((state: any) => state.accountReducer);
-  // const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
-  // const { pathname } = useLocation();
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
+  const { pathname } = useLocation();
 
   return (
-    <div
-      id="wd-courses-navigation"
-      className="wd list-group fs-5 rounded-0 me-4"
-    >
-      <Link
-        to="/Kambaz/Account/Signin"
-        id="wd-course-home-link"
-        className="list-group-item active border border-0"
-      >
-        Signin
-      </Link>
-      <Link
-        to="/Kambaz/Account/Signup"
-        id="wd-course-modules-link"
-        className="list-group-item text-danger border border-0"
-      >
-        Signup
-      </Link>
+    // <div
+    //   id="wd-courses-navigation"
+    //   className="wd list-group fs-5 rounded-0 me-4"
+    // >
+    //   <Link
+    //     to="/Kambaz/Account/Signin"
+    //     id="wd-course-home-link"
+    //     className="list-group-item active border border-0"
+    //   >
+    //     Signin
+    //   </Link>
+    //   <Link
+    //     to="/Kambaz/Account/Signup"
+    //     id="wd-course-modules-link"
+    //     className="list-group-item text-danger border border-0"
+    //   >
+    //     Signup
+    //   </Link>
 
-      <Link
-        to="/Kambaz/Account/Profile"
-        id="wd-course-piazza-link"
-        className="list-group-item text-danger border border-0"
-      >
-        Profile
-      </Link>
+    //   <Link
+    //     to="/Kambaz/Account/Profile"
+    //     id="wd-course-piazza-link"
+    //     className="list-group-item text-danger border border-0"
+    //   >
+    //     Profile
+    //   </Link>
+    // </div>
+    <div id="wd-account-navigation" className="list-group me-3">
+      {links.map((link) => (
+        <Link
+          key={link}
+          to={`/Kambaz/Account/${link}`}
+          className={`list-group-item ${active(link)}`}
+        >
+          {" "}
+          {link}{" "}
+        </Link>
+      ))}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link
+          to={`/Kambaz/Account/Users`}
+          className={`list-group-item ${active("Users")}`}
+        >
+          {" "}
+          Users{" "}
+        </Link>
+      )}
     </div>
   );
 }
